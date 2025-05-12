432184da01ccfa3863897ff902e02bbcf6e6ebd1
vishwakarma-sakshi <vishwkarmasakshi14@gmail.com>
2025-05-13 00:27:37 +0530

    Create two-sum.js

---
+++ two-sum.js
@@ -0,0 +1,17 @@
+function twoSum(nums, target) {
+    const map = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (map.has(complement)) {
+            return [map.get(complement), i];
+        }
+        map.set(nums[i], i);
+    }
+    return [];
+}
+
+// Example usage:
+const nums = [2, 7, 11, 15];
+const target = 9;
+console.log(twoSum(nums, target)); // Output: [0, 1]
